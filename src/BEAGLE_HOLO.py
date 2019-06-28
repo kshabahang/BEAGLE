38ca1a5b9a43f31739317964702808f55befbd93
kevin <kshabahang@gmail.com>
2019-06-28 16:08:51 +1000

    permutation coding seems to work, but one can never be too sure

--- src/BEAGLE_HOLO.py
+++ src/BEAGLE_HOLO.py
@@ -18,43 +18,6 @@ def ccorr(a, b):
     return cconv(np.roll(a[::-1], 1), b)
 
 
-
-
-
-class BIND_NODE(object):
-    def __init__(self, h, l = None, r = None, parent=None):
-        self.h = h #height
-        self.l = l #left-child
-        self.r = r #right-child
-
-
-
-
-
-
-class BIND(object):
-    def __init__(self, K, s, i, PHI):
-        self.K = K    # max number of ngrams
-        self.s = s    # token sequence
-        self.i = i    # index of target item to encode
-        self.PHI= PHI # place-holder for the target item
-        self.l = len(s) #number of tokens in the sequence
-        self.root = BIND_NODE(0)
-
-    def recurse_left(self, ):
-        #
-
-    def recurse_right(self, ):
-        #
-
-
-
-
-
-
-
-
-
 class BEAGLE_HOLO(Model):
     def __init__(self, params, hparams, QaAs=None):
         super(BEAGLE_HOLO, self).__init__(hparams, QaAs)
@@ -68,7 +31,6 @@ class BEAGLE_HOLO(Model):
         ###initialize order vectors
         self.O = []
         self.N      = hparams["NFEATs"]
-        self.K      = hparams["NGRAMS"]
         self.SD     = 1.0/np.sqrt(self.N)
 
         f = open("../rsc/STOPLIST.txt", "r")
@@ -82,6 +44,17 @@ class BEAGLE_HOLO(Model):
         self.p1 = deepcopy(perm)
         np.random.shuffle(perm)
         self.p2 = deepcopy(perm)
+
+#        self.E1_map = dict(zip([i for i in xrange(self.N)], np.random.permutation(self.N)))
+#        self.E2_map = dict(zip([i for i in xrange(self.N)], np.random.permutation(self.N)))
+#        self.D1_map = {self.E1_map[i]:i for i in xrange(self.N)} 
+#        self.D2_map = {self.E2_map[i]:i for i in xrange(self.N)}
+#    
+#        self.E1 = lambda a : np.array([a[self.E1_map[i]] for i in xrange(self.N)])
+#        self.E2 = lambda a : np.array([a[self.E2_map[i]] for i in xrange(self.N)])
+#        self.D1 = lambda a : np.array([a[self.D1_map[i]] for i in xrange(self.N)])
+#        self.D2 = lambda a : np.array([a[self.D2_map[i]] for i in xrange(self.N)])
+
 
     def bind(self, a, b):
         return cconv(a[self.p1], b[self.p2])
@@ -109,11 +82,12 @@ class BEAGLE_HOLO(Model):
         window = window.split()
         N = self.N
         I = self.I
+        WINDOW_LIM = self.hparams["CONTEXT_WINDOW"] + 1
         for i in xrange(len(window)):
             if window[i] not in self.STOPLIST:
                 wi = window[i]
                 for j in xrange(len(window)):
-                    if j != i and window[j] not in self.STOPLIST:
+                    if j != i and window[j] not in self.STOPLIST and np.abs(i - j) < WINDOW_LIM:
                         wj = window[j]
                         self.C[I[wi]] += self.E[I[wj]]
 
@@ -123,7 +97,19 @@ class BEAGLE_HOLO(Model):
         I = self.I
         for i in xrange(len(window)):
             wi = window[i]
-            self.O[I[wi]] += self.ngram_bind(window, i)
+            self.O[I[wi]] += self.RP_bind(window, i)
+
+    def RP_bind(self, window, i):
+        o = np.zeros(self.N)
+        I = self.I
+        WINDOW_LIM = self.hparams["ORDER_WINDOW"] + 1
+        for j in xrange(len(window)):
+            if WINDOW_LIM > j - i > 0:
+                o += self.perm_n(self.E[I[window[j]]], j-i ) #forward associations
+            elif -WINDOW_LIM < j - i < 0:
+                o += self.invperm_n(self.E[I[window[j]]], i - j)
+        return o
+
 
 
     def normalize_context(self):
@@ -141,52 +127,6 @@ class BEAGLE_HOLO(Model):
                 self.O[i] = self.O[i]/vlen  
 
 
-    def ngram_bind(self, window, i):
-        K = self.K #number of ngrams
-        N = self.N #number of features
-        I = self.I 
-        L = len(window)
-        bindings = {"PHI":deepcopy(self.PHI)}
-        v = np.zeros(N)
-        for n in xrange(2, K+1): #for each ngram level
-            ngrams = zip(*[window[j:] for j in range(n)])
-            ngrams_str = ''
-            for j in xrange(len(ngrams)):
-                if window[i] in ngrams[j]:
-                    
-                    ngram = list(ngrams[j])
-                    nless1gram = ' '.join(ngram[:n-1])
-                    if nless1gram in bindings: #recylce
-                        bindings[' '.join(ngram)] = self.bind(bindings[nless1gram], self.E[I[ngram[n-1]]])
-                    else: #create anew
-                        w = np.zeros(N)
-                        for l in xrange(len(ngram)-1):
-                            if ngram[l] == window[i]:
-                                v1 = bindings["PHI"]
-                                a = "PHI"
-                            else:
-                                v1 = self.E[I[ngram[l]]]
-                                a = ngram[l]
-                            if ngram[l+1] == window[i]:
-                                v2 = bindings["PHI"]
-                                b = "PHI"
-                            else:
-                                v2 = self.E[I[ngram[l+1]]]
-                                b = ngram[l]
-
-                            w += self.bind(v1, v2)
-                            ngrams_str += " ( {} * {} ) ".format(a, b)
-                        wlen = np.linalg.norm(w)
-                        if wlen > 0:
-                            w = w/wlen
-                        bindings[' '.join(ngram)] = w
-                        v += w
-                        print ngrams_str
-        vlen = np.linalg.norm(v)
-        if vlen > 0:
-            return v/vlen
-        else:
-            return v
 
     def compute_lexicon(self):
         '''combines context and order information
@@ -214,11 +154,21 @@ class BEAGLE_HOLO(Model):
         strengths = sorted(zip(map(lambda u : np.dot(u, v), self.C), self.vocab))[::-1]
         for i in xrange(10):
             print round(strengths[i][0], 7), strengths[i][1]
-        return strengths 
+        return strengths
 
+    def perm_n(self, x, n):
+        y = np.zeros(self.N)
+        for i in xrange(self.N):
+            y[(i+n)%self.N] = x[i]
+        return y
 
+    def invperm_n(self, y, n):
+        x = np.zeros(self.N)
+        for i in xrange(self.N):
+            x[(i-n)] = y[i]
+        return x
 
-    def sim_order(self, w):
+    def sim_order(self, w, n=0):
         I = self.I
         if type(w) == str and w in self.vocab:
             v = self.O[I[w]]
@@ -227,6 +177,11 @@ class BEAGLE_HOLO(Model):
         else:
             print "ERROR: the token, {}, is not known".format(w)
             return -1
+
+        if n < 0:
+            v = self.perm_n(v, -n)
+        if n > 0:
+            v = self.invperm_n(v, n)
 
         strengths =  sorted(zip(map(lambda u : np.dot(u, v), self.O), self.vocab))[::-1]
         for i in xrange(10):
@@ -253,56 +208,69 @@ class BEAGLE_HOLO(Model):
 
 if __name__ == "__main__":
     params = []
-    hparams = {"NFEATs":2048,  "NGRAMS":7}
+    hparams = {"NFEATs":1024,  "ORDER_WINDOW":7, "CONTEXT_WINDOW":7}
 
     beagle = BEAGLE_HOLO(params, hparams)
 
     ##load corpus
-    f = open("../rsc/BaseballWiki.txt", "r")
+    f = open("../rsc/tasaClean.txt", "r")
     corpus = f.readlines()
     f.close()
     corpus = [corpus[i].strip() for i in xrange(len(corpus))]
-#    pbar = ProgressBar(maxval = len(corpus)).start()
-#    for i in xrange(len(corpus)):
-#        beagle.update_vocab(corpus[i])
-#        beagle.learn_context(corpus[i])
-#        beagle.learn_order(corpus[i])
-#        pbar.update(i+1)
-#    beagle.normalize_context()
-#    beagle.normalize_order()
-#    beagle.compute_lexicon()
-#    sims = beagle.sim('batter')
+
+    corpus = ["a b c d e f g", "A B C D E F G", "1 2 3 4 5 6 7 8", "the cat was sitting on the mat"]
+    
+    pbar = ProgressBar(maxval = len(corpus)).start()
+    for i in xrange(len(corpus)):
+        beagle.update_vocab(corpus[i])
+        beagle.learn_context(corpus[i])
+        beagle.learn_order(corpus[i])
+        pbar.update(i+1)
+    beagle.normalize_context()
+    beagle.normalize_order()
+    beagle.compute_lexicon()
+
+    print "Testing retrieval using serial order with vector for letter D"
+
+    for i in xrange(1,4):
+        print "*"*72
+        print "lag {}".format(i)
+        print "Forward"
+        a = beagle.sim_order("D", i)
+        print ""
+        print "Backward"
+        a = beagle.sim_order("D", -i)
 
         
 
-    vocab = "A B C".split()
-    n = 1024
-    entries = {vocab[i]:np.random.normal(0.0, 1.0/np.sqrt(n), n) for i in xrange(len(vocab))}
-    sent = "A B C".split()
-    PHI = np.random.normal(0.0, 1.0/np.sqrt(n), n)
-
-    E1_map = dict(zip([i for i in xrange(n)], np.random.permutation(n)))
-    E2_map = dict(zip([i for i in xrange(n)], np.random.permutation(n)))
-    D1_map = {E1_map[i]:i for i in xrange(n)} 
-    D2_map = {E2_map[i]:i for i in xrange(n)}
-
-    E1 = lambda a : np.array([a[E1_map[i]] for i in xrange(n)])
-    E2 = lambda a : np.array([a[E2_map[i]] for i in xrange(n)])
-    D1 = lambda a : np.array([a[D1_map[i]] for i in xrange(n)])
-    D2 = lambda a : np.array([a[D2_map[i]] for i in xrange(n)])
-
-    
-    #encode order for B in position 1
-    idx = sent.index("B")
-    m_b = np.zeros(n)
-    m_b += cconv(E1(entries[sent[idx-1]]), E2(PHI)) #2-gram: A_
-    m_b += cconv(E1(PHI), E2(entries[sent[idx+1]])) #2-gram: _C
-    m_b += cconv(E1( cconv(E1(entries[sent[idx-1]]), E2(PHI)) ), E2(entries[sent[idx+1]])) #3-gram: A_C
-    
-    C_hat = D2(ccorr(E1(PHI), m_b)) #what vector succeeds B based on _C
-    print np.dot(C_hat, entries["C"])
-    C_hat = D2( ccorr( E1(cconv(E1(entries["A"]), E2(PHI))  ), m_b)) #what vector succeeds B, given A preceded it?
-    print np.dot(C_hat, entries["C"])
+#    vocab = "A B C".split()
+#    n = 1024
+#    entries = {vocab[i]:np.random.normal(0.0, 1.0/np.sqrt(n), n) for i in xrange(len(vocab))}
+#    sent = "A B C".split()
+#    PHI = np.random.normal(0.0, 1.0/np.sqrt(n), n)
+#
+#    E1_map = dict(zip([i for i in xrange(n)], np.random.permutation(n)))
+#    E2_map = dict(zip([i for i in xrange(n)], np.random.permutation(n)))
+#    D1_map = {E1_map[i]:i for i in xrange(n)} 
+#    D2_map = {E2_map[i]:i for i in xrange(n)}
+#
+#    E1 = lambda a : np.array([a[E1_map[i]] for i in xrange(n)])
+#    E2 = lambda a : np.array([a[E2_map[i]] for i in xrange(n)])
+#    D1 = lambda a : np.array([a[D1_map[i]] for i in xrange(n)])
+#    D2 = lambda a : np.array([a[D2_map[i]] for i in xrange(n)])
+#
+#    
+#    #encode order for B in position 1
+#    idx = sent.index("B")
+#    m_b = np.zeros(n)
+#    m_b += cconv(E1(entries[sent[idx-1]]), E2(PHI)) #2-gram: A_
+#    m_b += cconv(E1(PHI), E2(entries[sent[idx+1]])) #2-gram: _C
+#    m_b += cconv(E1( cconv(E1(entries[sent[idx-1]]), E2(PHI)) ), E2(entries[sent[idx+1]])) #3-gram: A_C
+#    
+#    C_hat = D2(ccorr(E1(PHI), m_b)) #what vector succeeds B based on _C
+#    print np.dot(C_hat, entries["C"])
+#    C_hat = D2( ccorr( E1(cconv(E1(entries["A"]), E2(PHI))  ), m_b)) #what vector succeeds B, given A preceded it?
+#    print np.dot(C_hat, entries["C"])
 
 
 
